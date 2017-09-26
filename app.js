60e4aabbeee1ea5891afce83568040f04222f546
Akintunde Thomas <tsatm00@yahoo.com>
2017-09-25 22:30:03 -0400

    first push

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var blog = require('./routes/blog');
-var portfolio = require('./routes/portfolio');
+//var portfolio = require('./routes/portfolio');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/blog', blog);
-app.use('/portfolio', portfolio);
+//app.use('/portfolio', portfolio);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router  = express.Router();
-
-router.get('/', function(request, response) {
-	response.render('portfolio/index');
-});
-
-module.exports = router;
+// var express = require('express');
+// var router  = express.Router();
+//
+// router.get('/', function(request, response) {
+// 	response.render('portfolio/index');
+// });
+//
+// module.exports = router;
